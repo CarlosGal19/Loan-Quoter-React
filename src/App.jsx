a21165548a9fc5f8b51b0478d152af05dd294df1
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-23 19:07:16 -0600

    feat: Add input range

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>
+      <input type="range" className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"/>
     </div>
   )
 }
