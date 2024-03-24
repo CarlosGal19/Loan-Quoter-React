8e88609e9baeacf2da344f215e16fadcd2e553a8
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-23 19:29:20 -0600

    feat: Add min and max value to range input

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   // The useState function returns an array with two elements. The first element is the current state value, and the second element is a function that allows you to update it.
   const [amount, setAmount] = useState(10000);
 
+  const min=0;
+  const max=20000;
+  const step=100;
+
   // Handle is a function that will be called when the input value changes. It is a standard name for this kind of function.
   function handleChange(e) {
     setAmount(e.target.value);
@@ -17,6 +21,10 @@ function App() {
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange={handleChange}
+        min={min}
+        max={max}
+        step={step}
+        value={amount}
       />
       <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{amount}</p>
     </div>
