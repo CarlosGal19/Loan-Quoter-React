a02b7ca415161d5aa6164f1f05eb0d987bccc231
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-23 19:03:29 -0600

    feat: Add component that shows a message to user

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
 
   return (
-    <h1>Hello world</h1>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+        <h1 className="text-4xl font-bold text-center text-gray-500">
+            How much <span className="text-indigo-600">money</span> do you need?
+        </h1>
+    </header>
+  );
+}
+
+export default Header;
