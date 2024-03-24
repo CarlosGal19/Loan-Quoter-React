729d128faf6d344f666c9c0f8c85a3df716fa5f9
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-23 19:25:22 -0600

    feat: Show number of range input using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import Header from "./components/Header";
 
 function App() {
+  // The useState function returns an array with two elements. The first element is the current state value, and the second element is a function that allows you to update it.
+  const [amount, setAmount] = useState(10000);
+
+  // Handle is a function that will be called when the input value changes. It is a standard name for this kind of function.
+  function handleChange(e) {
+    setAmount(e.target.value);
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      <Header/>
-      <input type="range" className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"/>
+      <Header />
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+      />
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{amount}</p>
     </div>
   )
 }
